43aef9c24c18af856160c57fa05b17502dbd207e
FSR <your_email@fpshami@gmail.com>
2021-02-27 17:14:30 -0600

    v2

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6efd18b86bf1b2d5c2e4e7b2817c93bd"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAy1c-bXUlWV_xIj0NXhtxuu_z_e0NK_yo"
